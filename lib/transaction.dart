5062f09258a364a4963fc27eea9c0c725299e607
petarsipka <petarsipka90@gmail.com>
2025-04-07 21:01:53 +0200

    first commit

---
+++ lib/transaction.dart
@@ -0,0 +1,40 @@
+import 'package:hive/hive.dart';
+
+part 'transaction.g.dart';
+
+@HiveType(typeId: 0)
+class Transaction extends HiveObject {
+  @HiveField(0)
+  final String id;
+  @HiveField(1)
+  final String title;
+  @HiveField(2)
+  final double amount;
+  @HiveField(3)
+  final DateTime date;
+
+  Transaction({
+    required this.id,
+    required this.title,
+    required this.amount,
+    required this.date,
+  });
+
+  Map<String, dynamic> toJson() {
+    return  {
+      'id': id,
+      'title': title,
+      'amount': amount,
+      'date': date.toIso8601String(),
+    };
+  }
+
+  factory Transaction.fromJson(Map<String, dynamic> json) {
+    return Transaction(
+      id: json['id'],
+      title: json['title'],
+      amount: json['amount'],
+      date: DateTime.parse(json['date']),
+    );
+  }
+}
